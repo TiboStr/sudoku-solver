0653ec202a7b2e4ab5f9e662fb4521aecbb6efd5
Tibo Stroo <tibo.stroo@hotmail.be>
2021-06-30 11:15:19 +0200

    handle user input

--- solver.py
+++ solver.py
@@ -1,6 +1,9 @@
+import re
+
+
 class Cell:
-    def __init__(self, row, col, block, x, y):
-        self.value = "."
+    def __init__(self, row, col, block, x, y, value):
+        self.value = value
         self.row = row
         self.column = col
         self.block = block
@@ -16,6 +19,11 @@ class Cell:
     def __str__(self):
         return str(self.value)
 
+    def value_is_changeable(self, value):
+        if not isinstance(value, int):  # you may always fill in e.g "*" of "." as a placeholder
+            return True
+        return not self.row.has_value(value) and not self.column.has_value(value) and not self.block.has_value(value)
+
 
 class Container:  # Every cell is part of 3 containers: 1 row, 1 column and 1 block
     def __init__(self):
@@ -27,46 +35,97 @@ class Container:  # Every cell is part of 3 containers: 1 row, 1 column and 1 bl
     def can_add_cell(self, cell):
         return cell not in self.cells and len(self.cells) + 1 <= 9
 
+    def has_value(self, value):
+        return value in {cell.value for cell in self.cells}
+
 
 class Grid:
-
     def __init__(self):
-        rows = [Container() for _ in range(9)]
-        cols = [Container() for _ in range(9)]
-        blocks = [Container() for _ in range(9)]
+        self.rows = [Container() for _ in range(9)]
+        self.cols = [Container() for _ in range(9)]
+        self.blocks = [Container() for _ in range(9)]
 
         self.grid = []
 
-        for r in range(9):
+        for row in range(9):
             self.grid.append([])
-            for c in range(9):
+            for col in range(9):
                 # https://stackoverflow.com/a/20268305
-                self.grid[r].append(Cell(rows[r], cols[c], blocks[r // 3 * 3 + c // 3], r, c))
+                self.grid[row].append(
+                    Cell(self.rows[row], self.cols[col], self.blocks[row // 3 * 3 + col // 3], row, col, "*"))
+        print(self)
+        self.add_values()
+
+    def is_value(self, value):
+        return bool(re.fullmatch("[1-9]", str(value)))
+
+    def handle_input(self, target):
+        err_msg = f"Your input for a {target} must be an integer value in [1,9]"
+        good_input = False
+        value = None
+        while not good_input:
+            try:
+                value = int(input(f"Enter the {target}: "))
+                if not self.is_value(value):
+                    print(err_msg)
+                else:
+                    good_input = True
+            except ValueError:
+                print(err_msg)
+        return value
 
     def add_values(self):
         print("Fill the Sudoku grid")
-        print("Use format number@row,col (e.g 5@1,7 will place a 5 on row 1 and column 7")
+        go = "y"
+        while go.lower() == "y":
+            row = self.handle_input("row")
+            col = self.handle_input("column")
+            value = self.handle_input("value")
+
+            cell = self.grid[row - 1][col - 1]
+            if cell.value_is_changeable(value):
+                cell.value = value
+            else:
+                print(
+                    f"You cannot change the value in ({row}, {col}) to {value}"
+                    f", the value must be unique in the row, column and block")
+
+            print("\n", self)
+            go = input("Do you wish to continue? (y/n)")
+            while go.lower() not in ["y", "n"]:
+                go = input("Do you wish to continue? (y/n)")
+
+        self.complete_grid()
+
+    def complete_grid(self):
         pass
-        # hier dan input vragen
+
+    def recursion_manager(self, row, col, value):
+        pass
+
+    def next_index(self, current_row, current_col):
+        next_col = current_col + 1 % 8
+        next_row = current_row + 1 if next_col == 0 else current_row
+        return [next_row, next_col] if 0 <= next_row <= 8 else None
 
     def __str__(self):
-        out = "1 2 3 4 5 6 7 8 9".center(20, " ") + "\n"
+        out = " ".join([str(i) for i in range(1, 10)]).center(20, " ") + "\n"
         col_count = 1
 
         for r_index, r in enumerate(self.grid):
             if r_index % 3 == 0:
-                out += ("- " * 10).rstrip(" ") + "\n"
+                out += "+-----" * 3 + "+" + "\n"
 
             for c_index, c in enumerate(r):
                 if c_index % 3 == 0:
                     out = out.rstrip(" ") + "|"
-                out += c.value + " "
+                out += str(c.value) + " "
 
             out = out.rstrip(" ") + "| " + str(col_count) + "\n"
             col_count += 1
 
-        return (out + "- " * 10).rstrip(" ")
+        return out + "+-----" * 3 + "+"
 
 
-g = Grid()
-print(g)
+if __name__ == '__main__':
+    Grid()
